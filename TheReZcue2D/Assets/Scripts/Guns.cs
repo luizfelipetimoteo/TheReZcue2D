64f9604fe9d52bf171343cd40c84f0b9330efd5d
luizfelipetimoteo <felipeglom@gmail.com>
2020-07-05 00:03:10 -0300

    Scripts
    Player Script
    Gun Script

---
+++ TheReZcue2D/Assets/Scripts/Guns.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guns : MonoBehaviour
+{
+    ArmasLeves Glock;
+
+    void Start()
+    {
+        Glock = new ArmasLeves();
+        Glock.Peso = 1;
+        Glock.VelDisparo = 5;
+        Glock.TempRecarga = 4;
+    }
+    void Update()
+    {
+        if (Player.EstaComGlock == true && Player.Vivo == true)
+        {
+
+        }
+    }
+
+}
+public class ArmasLeves : Guns
+{
+
+    public int Peso;
+    public int VelDisparo;
+    public int TempRecarga;
+
+    public GameObject Bala;
+    public GameObject LocalTiro;
+
+
+    void Tiro()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Player.Vivo == true)
+        {
+
+        }
+    }
+    void Recarga()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && Player.Vivo == true)
+        {
+
+        }
+    }
+    void TempTiro()
+    {
+
+    }
+}
---
+++ TheReZcue2D/Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static bool EstaComGlock;
+    public static bool Vivo;
+    private int Speed;
+    private int Vida;
+
+    void Start()
+    {
+        EstaComGlock = false;
+        Vida = 100;
+        Vivo = true;
+        Speed = 3;
+    }
+    void Update()
+    {
+        Player_Move();
+        Player_Dead();
+    }
+
+    void Player_Move()
+    {
+        //Vertical
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,Speed*Time.deltaTime,0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -Speed * Time.deltaTime, 0);
+        }
+        //Horizontal
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Speed * Time.deltaTime, 0,0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Speed * Time.deltaTime, 0, 0);
+        }
+    }
+    void Player_Dead()
+    {
+        if (Vida <= 0)
+        {
+            Vida = 0;
+            Vivo = false;
+            Destroy(gameObject);
+        }
+    }
+}
